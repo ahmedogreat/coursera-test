10b63038e7700564030373692b3f83c905e00a60
Ahmed Ali Mohammed <getahmedali@gmail.com>
2020-12-20 19:15:56 +0100

    .

---
+++ module1-solution/assignment1-starter-code/app.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope', '$filter'];
+function LunchCheckController($scope, $filter) {
+  $scope.dishes = "";
+  $scope.msg = "";
+
+  $scope.checkLunch = function () {
+    var itemCount=0;
+    var arrDishes=$scope.dishes.trim().split(',');
+    for(var i=0; i<arrDishes.length; i++){
+      if(arrDishes[i]){
+        itemCount++;
+      }
+    }
+
+    if(itemCount > 3){
+      $scope.msg="Too much!";
+    }else if(itemCount > 0){
+      $scope.msg="Enjoy!";
+    }
+  };
+}
+
+})();
